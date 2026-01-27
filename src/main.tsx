851b22888b390d8b43c10ead02a4197b8e1458d3
JPazem <109220752+JPazem@users.noreply.github.com>
2026-01-27 16:01:14 +0100

    Enforce dark mode to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+document.documentElement.classList.add("dark");
+document.documentElement.style.colorScheme = "dark";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <InteractiveRLLab />
