b413e198b065aff3c8ec9a5a6a996808b2fde5c7
JPazem <109220752+JPazem@users.noreply.github.com>
2026-01-28 11:43:39 +0100

    Add DarkModeToggle to the app and choose some colors for each mode
    Step 3 of solving the dark mode issue: add the toggle to the app and set the colors in its parameters

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import './App.css'
 import InteractiveRLLab from './InteractiveRLLAb';
+import DarkModeToggle from "./components/ui/DarkModeToggle";
 
 export default function App() {
-  return <InteractiveRLLab />;
+  return (
+    <div className="min-h-screen bg-white dark:bg-slate-900 text-black dark:text-slate-100 transition-colors">
+      <div className="p-3 flex justify-end">
+        <DarkModeToggle />
+      </div>
+
+      <InteractiveRLLab />
+    </div>
+  );
 }
