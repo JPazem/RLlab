b7103d757ddcc6bd07bc401a1e1477eea85715f0
JPazem <109220752+JPazem@users.noreply.github.com>
2026-01-28 11:12:22 +0100

    Fix formatting of base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
  
 // https://vite.dev/config/
 export default defineConfig({
- base: "/InteractiveRLlab/"
+  base: "/InteractiveRLlab/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
